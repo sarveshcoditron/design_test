5d2e503c24436d2071e0344a7d1f52d0f70a9584
sarveshcoditron <sarvesh.ransubhe@coditron.com>
2025-01-03 20:57:34 +0530

    done

--- lib/src/sample_feature/sample_item_details_view.dart
+++ lib/src/sample_feature/sample_item_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
@@ -9,170 +10,247 @@ class SampleItemDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final m = MediaQuery.of(context);
+    Widget singleWallet = _buildWalletCard(
+      m,
+      mainWidget: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset(
+            "assets/images/hand.png",
+            height: 90,
+            width: 90,
+          ),
+          Expanded(
+            child: Column(
+              spacing: 6,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 6),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "Cash",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "2356  ",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Text(
+                        "This is the cash wallet",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "2356  ",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomWidget: Text(
+        '11th Sep 2023 to 11 Oct 2023',
+        style: const TextStyle(
+          color: Colors.grey,
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      buttonText: "More",
+      onPressed: () {},
+    );
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text('Cards & Wallets'),
         backgroundColor: Colors.grey[200],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          spacing: 12,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            _buildWalletCard(
-              m,
-              mainWidget: Image.asset(
-                "assets/images/credit_card.png",
-                fit: BoxFit.fitWidth,
-              ),
-              bottomWidget: RichText(
-                text: TextSpan(
-                  text: 'Card Status: ',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Inactive',
-                      style: const TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold,
-                      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            spacing: 12,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              _buildWalletCard(
+                m,
+                mainWidget: Image.asset(
+                  "assets/images/credit_card.png",
+                  fit: BoxFit.fitWidth,
+                ),
+                bottomWidget: RichText(
+                  text: TextSpan(
+                    text: '       Card Status: ',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
                     ),
-                  ],
-                ),
-              ),
-              buttonText: "Guide",
-              onPressed: () {
-                print("Button Pressed");
-              },
-            ),
-
-            // Lifeline Card
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(14),
-                border: Border.all(
-                  color: Colors.grey[300]!,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Row(
                     children: [
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Text("Activate your Lifeline Card"),
-                          ],
+                      TextSpan(
+                        text: 'Inactive',
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
-                      Image.asset(
-                        "assets/images/person.png",
-                        height: 120,
-                        fit: BoxFit.fitWidth,
-                      )
                     ],
                   ),
-                  Divider(),
-                ],
+                ),
+                buttonText: "Guide",
+                onPressed: () {},
               ),
-            ),
 
-            Text(
-              "Wallet",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-
-            _buildWalletCard(
-              m,
-              mainWidget: Row(
-                children: [
-                  Image.asset(
-                    "assets/images/hand.png",
-                    height: 90,
-                    width: 90,
+              // Lifeline Card
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(14),
+                  border: Border.all(
+                    color: Colors.grey[300]!,
                   ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  spacing: 8,
+                  children: [
+                    Row(
                       children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                "Cash",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(4),
+                            child: Column(
+                              spacing: 4,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text("Activate your Lifeline Card",
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold)),
+                                Text(
+                                  "₹ 3499/- Life Time",
+                                  style: TextStyle(
+                                    color: Colors.blue,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                            ),
-                            Text(
-                              "2356",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            )
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Text(
-                                "This is the cash wallet",
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.normal,
+                                Text(
+                                  "₹ 9999/- Year",
+                                  style: TextStyle(
+                                    color: Colors.red,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
+                                Text("Offer ends soon"),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(14),
+                                    ),
+                                    backgroundColor: Colors.blue[900],
+                                    minimumSize: const Size(160, 40),
+                                  ),
+                                  child: Text(
+                                    "Activate Now",
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              "2356",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.normal,
-                              ),
-                            )
-                          ],
+                          ),
                         ),
+                        Image.asset(
+                          "assets/images/person.png",
+                          height: 160,
+                          fit: BoxFit.fitWidth,
+                        )
                       ],
                     ),
-                  ),
-                ],
-              ),
-              bottomWidget: RichText(
-                text: TextSpan(
-                  text: 'Card Status: ',
-                  style: const TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0,
-                  ),
-                  children: [
-                    TextSpan(
-                      text: 'Inactive',
-                      style: const TextStyle(
-                        color: Colors.red,
+                    Divider(),
+                    Text(
+                      "Our Features",
+                      style: TextStyle(
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        spacing: 12,
+                        children: [
+                          _buildFeature1("Udhar Limit: ₹ 2376"),
+                          _buildFeature1("CP EMI Limit: ₹ 2376"),
+                          _buildFeature1("Udhar Limit: ₹ 2376"),
+                          _buildFeature1("CP EMI Limit: ₹ 2376"),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      spacing: 6,
+                      children: [
+                        _buildFeature2(
+                          "Udhar",
+                          "assets/images/money.svg",
+                        ),
+                        _buildFeature2(
+                          "CP EMI",
+                          "assets/images/money.svg",
+                        ),
+                        _buildFeature2(
+                          "Business Funds",
+                          "assets/images/money.svg",
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
-              buttonText: "More",
-              onPressed: () {
-                print("Button Pressed");
-              },
-            ),
-          ],
+
+              Text(
+                "Wallet",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              singleWallet,
+              singleWallet,
+              singleWallet,
+            ],
+          ),
         ),
       ),
     );
@@ -212,10 +290,79 @@ class SampleItemDetailsView extends StatelessWidget {
                     backgroundColor: const Color.fromARGB(255, 194, 169, 248),
                     minimumSize: const Size(160, 40),
                   ),
-                  child: Text(buttonText),
+                  child: Text(
+                    buttonText,
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
               ],
             )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFeature1(String m) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.all(
+          Radius.circular(6),
+        ),
+      ),
+      padding: EdgeInsets.all(4),
+      child: Text(
+        m,
+        style: TextStyle(
+          color: Colors.grey,
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFeature2(String text, String asset) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          spacing: 6,
+          children: [
+            Container(
+              height: 90,
+              width: 90,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                ),
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.all(
+                  Radius.circular(6),
+                ),
+              ),
+              padding: EdgeInsets.all(4),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SvgPicture.asset(asset),
+              ),
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+              maxLines: 1,
+            ),
           ],
         ),
       ),
@@ -235,7 +382,7 @@ class _Painter extends CustomPainter {
       end: Alignment.bottomCenter,
       colors: [
         Colors.white,
-        Colors.lightBlueAccent,
+        Colors.lightBlue[50]!,
       ],
     );
 
